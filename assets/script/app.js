721840f31069136c2247720b812e5782eb30e54d
MasterKelwin <mkelwin@outlook.com>
2022-06-05 11:55:08 -0300

    loop

---
+++ assets/script/app.js
@@ -0,0 +1,29 @@
+let $ = document.querySelector.bind(document);
+const player = $("#player");
+const obstaculo = $("#obstaculo");
+
+const jump = () => {    
+    player.classList.add('jump')
+
+    setTimeout(() => {
+        player.classList.remove('jump')
+    }, 600);
+}
+
+const loop = setInterval(() => {
+    const posicaoObstaculo = obstaculo.offsetLeft;
+    const posicaoPlayerString = window.getComputedStyle(player).bottom.replace('px', '');
+    const posicaoPlayer = Number(posicaoPlayerString);
+    console.log(posicaoPlayer)
+
+    if(posicaoObstaculo <= 180 && posicaoPlayer <= 150 && posicaoObstaculo >= 10) {
+
+        obstaculo.style.animation = 'none';
+        obstaculo.style.left = `${posicaoObstaculo}px`
+        player.style.animation = 'none';
+        player.style.bottom = `${posicaoPlayer}px`
+
+    }
+}, 10)
+
+document.addEventListener('keydown', jump);
