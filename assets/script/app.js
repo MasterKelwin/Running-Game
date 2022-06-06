bdeb7ec83d784c245a5c971d8735d040da8cf056
MasterKelwin <mkelwin@outlook.com>
2022-06-06 10:21:56 -0300

    ajustando placar

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,6 +1,9 @@
 let $ = document.querySelector.bind(document);
 const player = $("#player");
 const obstaculo = $("#obstaculo");
+const pontos = $("#pontos");
+let score = 0;
+let banana = false;
 
 const jump = () => {    
     player.classList.add('jump')
@@ -10,24 +13,40 @@ const jump = () => {
     }, 600);
 }
 
+
+
 const loop = setInterval(() => {
     const posicaoObstaculo = obstaculo.offsetLeft;
     const posicaoPlayerString = window.getComputedStyle(player).bottom.replace('px', '');
     const posicaoPlayer = Number(posicaoPlayerString);
     console.log(posicaoPlayer)
 
-    if(posicaoObstaculo <= 180 && posicaoPlayer <= 150 && posicaoObstaculo >= 10) {
-
+    const paraObstaculo = () => {
         obstaculo.style.animation = 'none';
         obstaculo.style.left = `${posicaoObstaculo}px`;
+    }
 
+    const paraPlayer = () => {
         player.style.animation = 'none';
         player.src = "assets/imgs/game-over.png";
-        //player.classList.remove('player');
-        //player.classList.add('game-over');
         player.style.bottom = `${posicaoPlayer}px`;
-        
+        player.style.left = '75px'
+        player.style.width = "100px"
     }
+
+    if(posicaoObstaculo <= 180 && posicaoPlayer <= 180 && posicaoObstaculo >= 10) {
+        paraObstaculo();
+        paraPlayer();
+    } else if (posicaoObstaculo <= 170 && posicaoObstaculo >= 100 && banana == false) {         
+            score++;
+            pontos.textContent = `PONTOS: ${score}`;
+            banana = true;
+    } else if (score >= pontos.textContent && banana == true) {
+        banana = false;
+    }
+
+
+
 }, 10)
 
 document.addEventListener('keydown', jump);
